ad7cd934c9c45ce76881cd21a4690175a978a173
lavahasif <lavahasif@gmail.com>
2020-10-27 21:41:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/fastsplash/OnboardingWithCenterAnimationActivity.java
@@ -0,0 +1,101 @@
+package com.example.fastsplash;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.ViewPropertyAnimatorCompat;
+
+public class OnboardingWithCenterAnimationActivity extends AppCompatActivity {
+    public static final int STARTUP_DELAY = 300;
+    public static final int ANIM_ITEM_DURATION = 1000;
+    public static final int ITEM_DELAY = 300;
+
+    private boolean animationStarted = false;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding_center);
+
+//        for(int i=0;i<5000;i++){
+//            if (i==4999){
+//                startActivity(new Intent(this, MainActivity.class));
+//            }
+//
+//        }
+        Boolean vas = false;
+
+        Intent intent = new Intent(this, MainActivity.class);
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                startActivity(intent);
+            }
+        }, 3000);
+//      while (vas==true){
+//
+//      }
+//        Intent intent = new Intent(this, MainActivity.class);
+//        startActivity(intent);
+
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+
+
+        if (!hasFocus || animationStarted) {
+            return;
+
+        }
+
+//    animate();
+
+        super.onWindowFocusChanged(hasFocus);
+    }
+
+    private void animate() {
+        ImageView logoImageView = (ImageView) findViewById(R.id.img_logo);
+        ViewGroup container = (ViewGroup) findViewById(R.id.container);
+
+        ViewCompat.animate(logoImageView)
+                .translationY(-250)
+                .setStartDelay(STARTUP_DELAY)
+                .setDuration(ANIM_ITEM_DURATION).setInterpolator(
+                new DecelerateInterpolator(1.2f)).start();
+
+        for (int i = 0; i < container.getChildCount(); i++) {
+            View v = container.getChildAt(i);
+            ViewPropertyAnimatorCompat viewAnimator;
+
+            if (!(v instanceof Button)) {
+                viewAnimator = ViewCompat.animate(v)
+                        .translationY(50).alpha(1)
+                        .setStartDelay((ITEM_DELAY * i) + 500)
+                        .setDuration(1000);
+            } else {
+                viewAnimator = ViewCompat.animate(v)
+                        .scaleY(1).scaleX(1)
+                        .setStartDelay((ITEM_DELAY * i) + 500)
+                        .setDuration(500);
+            }
+
+            viewAnimator.setInterpolator(new DecelerateInterpolator()).start();
+        }
+    }
+}
